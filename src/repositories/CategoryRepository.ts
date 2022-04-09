7c21b0900568d3d9672e42b08a6c6bc45254c3d0
Fernando Brino <fernandobrino11@gmail.com>
2022-04-09 15:48:49 -0300

    Apllying some SOLID concepts

--- src/repositories/CategoryRepository.ts
+++ src/repositories/CategoryRepository.ts
@@ -1,13 +1,10 @@
 import { Category } from "../model/Category";
+import {
+    ICategoriesRepository,
+    ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-// DTO => data transfer obejct
-
-interface ICreateCategoryDTO {
-    name: string;
-    description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
     constructor() {
